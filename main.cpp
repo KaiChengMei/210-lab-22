fb8c0553cb7282aaf284a2a859f274819ce4cde1
Kai-Cheng Mei <kennymei47@gmail.com>
2024-10-13 21:36:05 -0700

    1st

---
+++ main.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+using namespace std;
+
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+
+class DoublyLinkedList {
+private:
+    struct Node {
+        int data;
+        Node* prev;
+        Node* next;
+        Node(int val, Node* p = nullptr, Node* n = nullptr) {
+            data = val; 
+            prev = p;
+            next = n;
+        }
+    };
+
+    Node* head;
+    Node* tail;
+
+public:
+    // constructor
+    DoublyLinkedList() { head = nullptr; tail = nullptr; }
+
+    void push_back(int value) {
+        Node* newNode = new Node(value);
+        if (!tail)  // if there's no tail, the list is empty
+            head = tail = newNode;
+        else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void push_front(int value) {
+        Node* newNode = new Node(value);
+        if (!head)  // if there's no head, the list is empty
+            head = tail = newNode;
+        else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void delete_pos(int position) {
+        if (!head || position < 0) {
+            cout << "Invalid position.\n";
+            return;
+        }
+
+        Node* temp = head;
+
+        // Deleting head (position 0)
+        if (position == 0) {
+            head = head->next;
+            if (head) head->prev = nullptr;
+            else tail = nullptr;  // List becomes empty
+            delete temp;
+            return;
+        }
+
+        // Traverse to the desired position
+        for (int i = 0; i < position && temp; ++i) {
+            temp = temp->next;
+        }
+
+        if (!temp) {
+            cout << "Position exceeds list size.\n";
+            return;
+        }
+
+        // Deleting tail
+        if (temp == tail) {
+            tail = tail->prev;
+            tail->next = nullptr;
+            delete temp;
+            return;
+        }
+
+        // Deleting any node in the middle
+        temp->prev->next = temp->next;
+        if (temp->next) {
+            temp->next->prev = temp->prev;
+        }
+
+        delete temp;
+    }
+
+    void print() {
+        Node* current = head;
+        if (!current) return;
+        while (current) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    void print_reverse() {
+        Node* current = tail;
+        if (!current) return;
+        while (current) {
+            cout << current->data << " ";
+            current = current->prev;
+        }
+        cout << endl;
+    }
+
+    ~DoublyLinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+// Driver program
+int main() {
+    DoublyLinkedList list;
+    int size = rand() % (MAX_LS-MIN_LS+1) + MIN_LS;
+
+    for (int i = 0; i < size; ++i)
+        list.push_back(rand() % (MAX_NR-MIN_NR+1) + MIN_NR);
+    cout << "List forward: ";
+    list.print();
+
+    cout << "List backward: ";
+    list.print_reverse();
+
+    cout << "Deleting list, then trying to print.\n";
+    list.~DoublyLinkedList();
+    cout << "List forward: ";
+    list.print();
+
+    return 0;
+}
